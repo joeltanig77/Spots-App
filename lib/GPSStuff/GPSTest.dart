f9945fba036d83ada186c15facb4ab2e6b76ac68
Joel Tanig <joeltanig77@gmail.com>
2020-12-21 07:25:17 -0800

    GPSTest.dart fixed

---
+++ lib/GPSStuff/GPSTest.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GPSTest extends StatefulWidget {
+  @override
+  _GPSTestState createState() => _GPSTestState();
+}
+
+class _GPSTestState extends State<GPSTest> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
