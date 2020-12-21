9bde5495fbaa4e70fb25c7dc982e09e0fb34049d
Joel Tanig <joeltanig77@gmail.com>
2020-12-21 08:19:44 -0800

    added google maps to main page

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,28 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:spots_app/services/auth.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   // Reference our service class
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final Service _auth = Service();
+
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.amber[100],
-      appBar: AppBar(
-        title: Text("Spots",),
-        centerTitle: true,
-        backgroundColor: Colors.orange[300],
-        // Note, This is actions for the appbar like the log in button
-        actions: [
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            onPressed: () async{
-              await _auth.signOut();
-            },
-            label: Text("Log out"),
-          )
-        ],
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amber[100],
+        appBar: AppBar(
+          title: Text(
+            "Spots",
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.orange[300],
+          // Note, This is actions for the appbar like the log in button
+          actions: [
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+              label: Text("Log out"),
+            ),
+          ],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
       ),
     );
   }
