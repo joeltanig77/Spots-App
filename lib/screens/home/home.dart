2f2a6700342c213cd6ca32a9ab7a0a85a9a993d0
CreTheCrayon <christian.m.rondeau@gmail.com>
2021-01-04 15:58:16 -0800

    Fixed Card merging with FAB

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -240,7 +240,7 @@ class _HomeState extends State<Home> {
                           child: Padding(
                             padding: const EdgeInsets.all(8.0),
                             child: Text(
-                              'Dead Fish',
+                              'Dead Bird',
                             ),
                           ),
                         ),
