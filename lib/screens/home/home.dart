8723e2a015dbf47a934e1c440abb7909871d6575
CreTheCrayon <christian.m.rondeau@gmail.com>
2021-01-07 14:00:36 -0800

    Added a search bar on the home page (no functionality yet)

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -17,6 +17,7 @@ import 'package:spots_app/models/locations.dart';
 import 'package:spots_app/models/user.dart';
 import 'package:spots_app/screens/home/trade.dart';
 import 'package:spots_app/services/userInformationDatabase.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
 double long = -75.7009;
 double lat = 45.4236;
@@ -197,6 +198,7 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+    final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
     getLocal();
     setState(() {
 
@@ -290,6 +292,7 @@ class _HomeState extends State<Home> {
               });
             },
           ),
+
           AnimatedPositioned(
             bottom: finishedPillPosition,
             right: 0,
@@ -492,7 +495,57 @@ class _HomeState extends State<Home> {
               }),
             ),
           ),
+
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0,7,0,0),
+            child: FloatingSearchBar(
+              hint: 'Search...',
+              scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+              transitionDuration: const Duration(milliseconds: 800),
+              transitionCurve: Curves.easeInOut,
+              physics: const BouncingScrollPhysics(),
+              axisAlignment: isPortrait ? 0.0 : -1.0,
+              openAxisAlignment: 0.0,
+              height: 37,
+              maxWidth: isPortrait ? 250 : 500,
+              debounceDelay: const Duration(milliseconds: 500),
+              onQueryChanged: (query) {
+                // Call your model, bloc, controller here.
+              },
+              // Specify a custom transition to be used for
+              // animating between opened and closed stated.
+              transition: CircularFloatingSearchBarTransition(),
+              actions: [
+                FloatingSearchBarAction(
+                  showIfOpened: false,
+                  child: CircularButton(
+                    icon: const Icon(Icons.place),
+                    onPressed: () {},
+                  ),
+                ),
+                FloatingSearchBarAction.searchToClear(
+                  showIfClosed: false,
+                ),
+              ],
+              builder: (context, transition) {
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Material(
+                    color: Colors.white,
+                    elevation: 4.0,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: Colors.accents.map((color) {
+                        return Container(height: 112, color: color);
+                      }).toList(),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
         ]),
+
         floatingActionButton: Padding(
           padding: const EdgeInsets.only(left: 32.0),
           child: Align(
