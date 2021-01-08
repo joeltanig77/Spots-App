db37f9eceabcc921850a6f74c7f72eefe4228b19
CreTheCrayon <christian.m.rondeau@gmail.com>
2021-01-07 20:11:49 -0800

    Added a more detailed placeholder Trade page based on what our stretch goals are for it.

--- lib/screens/home/trade.dart
+++ lib/screens/home/trade.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:spots_app/services/auth.dart';
 import 'package:spots_app/screens/home/home.dart';
@@ -21,118 +22,187 @@ class _TradePageState extends State<TradePage>  {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.amber[100],
-        appBar: AppBar(
-          backgroundColor: Colors.orange[300],
-          // Note, This is actions for the appbar like the log in button
-          actions: [
-            FlatButton(
-              padding: EdgeInsets.only(right: 60.0),
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
-              onPressed: () async {},
-              child: Text("Trade",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                  )),
-            ),
-            FlatButton(
-              padding: EdgeInsets.only(right: 60.0),
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
-              onPressed: () async {
-                Navigator.pop(
-                  context,
-                  PageRouteBuilder(
-                    pageBuilder: (context, animation1, animation2) =>
-                        Home(null, null, null),
-                    transitionDuration: Duration(seconds: 0),
-                  ),
-                );
-              },
-              child: Text("Map",
-                  style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 20.0,
-                  )),
-            ),
-            FlatButton(
-              padding: EdgeInsets.only(right: 60.0),
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
-              onPressed: () async {
-                Navigator.pop(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          backgroundColor: Colors.amber[100],
+          appBar: AppBar(
+            backgroundColor: Colors.orange[300],
+            // Note, This is actions for the appbar like the log in button
+            actions: [
+              FlatButton(
+                padding: EdgeInsets.only(right: 60.0),
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                onPressed: () async {},
+                child: Text("Trade",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    )),
+              ),
+              FlatButton(
+                padding: EdgeInsets.only(right: 60.0),
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                onPressed: () async {
+                  Navigator.pop(
                     context,
                     PageRouteBuilder(
-                    pageBuilder: (context, animation1, animation2) =>
-                    Home(null, null, null),
-                transitionDuration: Duration(seconds: 0),
+                      pageBuilder: (context, animation1, animation2) =>
+                          Home(null, null, null),
+                      transitionDuration: Duration(seconds: 0),
                     ),
-                    );
-                Navigator.push(
-                  context,
-                  PageRouteBuilder(
-                    pageBuilder: (context, animation1, animation2) => ProfilePage(null, "", ""),
-                    transitionDuration: Duration(seconds: 0),
-                  ),
-                );
-              },
-              child: Text("Profile",
-                  style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 20.0,
-                  )),
-            ),
-          ],
-        ),
-        body: Stack(
-          children: [
-          Align(
-          alignment: Alignment.center,
-          child: Text(
-              "Coming Soon*...",
-              style: TextStyle(
-                color: Colors.black54,
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-              )
-          ),
-        ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                  "*soon indicates sometime in the eventual future, whether it be in this universe or some other",
-                  style: TextStyle(
-                    color: Colors.black54,
-                    fontSize: 12,
-                  )
+                  );
+                },
+                child: Text("Map",
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 20.0,
+                    )),
               ),
+              FlatButton(
+                padding: EdgeInsets.only(right: 60.0),
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                onPressed: () async {
+                  Navigator.pop(
+                      context,
+                      PageRouteBuilder(
+                      pageBuilder: (context, animation1, animation2) =>
+                      Home(null, null, null),
+                  transitionDuration: Duration(seconds: 0),
+                      ),
+                      );
+                  Navigator.push(
+                    context,
+                    PageRouteBuilder(
+                      pageBuilder: (context, animation1, animation2) => ProfilePage(null, "", ""),
+                      transitionDuration: Duration(seconds: 0),
+                    ),
+                  );
+                },
+                child: Text("Profile",
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 20.0,
+                    )),
+              ),
+            ],
+            bottom: TabBar(
+              indicatorColor: Colors.white,
+              tabs: [
+                Tab(icon: Icon(Icons.explore)),
+                Tab(icon: Icon(Icons.mail)),
+              ],
             ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Container(
-                width: MediaQuery.of(context).size.width / 8,
-                height: MediaQuery.of(context).size.height,
-                child: GestureDetector(
-                    onHorizontalDragEnd: (DragEndDetails details) {
-                      if (details.primaryVelocity > 0) {
-
-                      } else if (details.primaryVelocity < 0) {
-                        Navigator.pop(
-                          context,
-                          PageRouteBuilder(
-                            pageBuilder: (context, animation1, animation2) => Home(null, null, null),
-                            transitionDuration: Duration(seconds: 0),
+          ),
+          body: Stack(
+            children: [
+              TabBarView(
+                children: [
+                  GridView.count(
+                    primary: false,
+                    padding: const EdgeInsets.all(20),
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 2,
+                    children: <Widget>[
+                      Flexible(
+                        child: Card(
+                          color: Colors.amber,
+                          child: Column(
+                            children: [
+                              Image.asset('images/beach.jpg'),
+                              Flexible(child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                                  child: Text(
+                                      'Happy Place',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18.0,
+                                      ),
+                                  ),
+                                ),
+                              )
+                              ),
+                              Flexible(
+                                child: Row(
+                                  children: [
+                                    Flexible(
+                                      child: Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: FlatButton(
+                                          textColor: Colors.yellow[300],
+                                          onPressed: (){},
+                                          child: Text('TRADE'),
+                                        ),
+                                      ),
+                                    ),
+                                    Flexible(
+                                      child: Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: FlatButton(
+                                          textColor: Colors.yellow[300],
+                                          onPressed: (){},
+                                          child: Text('TEXT'),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
-                        );
+                        ),
+                      ),
+                    ],
+                  ),
+                  Center(
+                    child: Column(
+                      children: [
+                        Image.asset('images/sampleChat.jpg'),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        Text(
+                            ' Sample Chat Above\nNot Yet Implemented',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  width: MediaQuery.of(context).size.width / 8,
+                  height: MediaQuery.of(context).size.height,
+                  child: GestureDetector(
+                      onHorizontalDragEnd: (DragEndDetails details) {
+                        if (details.primaryVelocity > 0) {
+
+                        } else if (details.primaryVelocity < 0) {
+                          Navigator.pop(
+                            context,
+                            PageRouteBuilder(
+                              pageBuilder: (context, animation1, animation2) => Home(null, null, null),
+                              transitionDuration: Duration(seconds: 0),
+                            ),
+                          );
+                        }
                       }
-                    }
+                  ),
                 ),
               ),
-            ),
-          ]
+            ]
+          ),
         ),
       ),
     );
