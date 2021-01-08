48dda75ef1b1e1b69bdb2fddc323b60daeac9823
Desi A <desidesumedia@gmail.com>
2021-01-07 16:26:26 -0800

    Updated default image.

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -42,7 +42,7 @@ List<Marker> cloudMarkers=[];
 String user;
 String bio;
 File userImage;
-String currentImageUrl="https://firebasestorage.googleapis.com/v0/b/spots-80f7d.appspot.com/o/MbAKzrkyoBZo47J6H2CCFulLnWS2%2FMy%20Car?alt=media&token=98c2d877-f983-4848-a544-1d6524ac5b1a";
+String currentImageUrl= "https://firebasestorage.googleapis.com/v0/b/spots-80f7d.appspot.com/o/MbAKzrkyoBZo47J6H2CCFulLnWS2%2FThis%20should%20be%20in%20my%20own%20folder!?alt=media&token=7404a6a8-5977-4050-b500-f338e13f2a3c";
 String searchQuery;
 List<String> queryLocations=[];
 
@@ -621,7 +621,7 @@ class _HomeState extends State<Home> {
               backgroundColor: Colors.orange[300],
               onPressed: () {
                 if (!activeMarker) {
-                  currentImageUrl="https://firebasestorage.googleapis.com/v0/b/spots-80f7d.appspot.com/o/MbAKzrkyoBZo47J6H2CCFulLnWS2%2FMy%20Car?alt=media&token=98c2d877-f983-4848-a544-1d6524ac5b1a";;
+                  currentImageUrl="https://firebasestorage.googleapis.com/v0/b/spots-80f7d.appspot.com/o/MbAKzrkyoBZo47J6H2CCFulLnWS2%2FThis%20should%20be%20in%20my%20own%20folder!?alt=media&token=7404a6a8-5977-4050-b500-f338e13f2a3c";
                   activeMarker=true;
                   locationName="";
                   desc="";
